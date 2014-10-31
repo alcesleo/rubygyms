42fbc51a233d520a9f595a8631f20eba42689f26
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 19:22:27 +0100

    Added refactor note

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  # TODO: Refactor
   def create
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
