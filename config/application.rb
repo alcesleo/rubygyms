bcb5f4c43a82022fb5d20a306fe2ee99a3fdbc2f
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 12:59:09 +0100

    Removed some annoying empty files and disabled some generators

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ module Rubygyms
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Disable some annoying generators
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
