e947dc8783f744d42a14123dab149538df66f419
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 18:46:31 +0100

    Tested login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe SessionsController, :type => :controller do
     end
   end
 
+  describe "POST create" do
+    it "logs a user in" do
+      Factory(:user, email: 'test@email.com', password: 'password')
+
+      post :create, user: { email: 'test@email.com', password: 'password' }
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "re-renders the template if the credentials are wrong" do
+      Factory(:user, email: 'test@email.com', password: 'password')
+
+      post :create, user: { email: 'test@email.com', password: 'wrong' }
+
+      expect(response).to render_template(:new)
+    end
+  end
+
 end
