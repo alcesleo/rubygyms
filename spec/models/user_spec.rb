c3af50a8a212b570bf5f0613973a8421c21c60c8
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 18:21:33 +0100

    Fix a test bug and reformat user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,25 +6,29 @@ RSpec.describe User, :type => :model do
 
   it "has a full name" do
     user = Factory.build(:user, first_name: "John", last_name: "Doe")
+
     expect(user.full_name).to eq "John Doe"
   end
 
   it "reports the correct role when owner" do
     user = Factory.build(:owner)
+
     expect(user).not_to be_member
     expect(user).to be_owner
   end
 
   it "reports the correct role when member" do
     user = Factory.build(:member)
+
     expect(user).to be_member
     expect(user).not_to be_owner
   end
 
   it "can be authenticated" do
     user = Factory.create(:user, password: 'password')
+
     expect(user.authenticate('wrong')).to eq false
-    expect(user.authenticate('password')).to eq true
+    expect(user.authenticate('password')).to eq user
   end
 
 
