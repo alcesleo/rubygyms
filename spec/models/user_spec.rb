e7fee9cc15a9b73854861f6c6b03e2db9d3167eb
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 14:05:34 +0100

    Sped up the tests by not hitting the db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe User, :type => :model do
   it { is_expected.to accept_values_for :email, "email@email.com" }
 
   it "has a full name" do
-    user = Factory(:user, first_name: "John", last_name: "Doe")
+    user = Factory.build(:user, first_name: "John", last_name: "Doe")
     expect(user.full_name).to eq "John Doe"
   end
 
   it "reports the correct role when owner" do
-    user = Factory(:owner)
+    user = Factory.build(:owner)
     expect(user).not_to be_member
     expect(user).to be_owner
   end
 
   it "reports the correct role when member" do
-    user = Factory(:member)
+    user = Factory.build(:member)
     expect(user).to be_member
     expect(user).not_to be_owner
   end
