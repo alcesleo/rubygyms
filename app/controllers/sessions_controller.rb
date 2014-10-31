378782f407555e499001da3ce4f10b830c6aa42b
Jimmy Börjesson <lagginglion@gmail.com>
2014-10-31 21:10:10 +0100

    Added logged out notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
   def log_out
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: 'Logged out!'
   end
 
 private
